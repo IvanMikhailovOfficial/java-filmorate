ce1986a293569578cfac2dd2f25ef5d97bf586ea
IvanMikhailovOfficial <ivanm54@yandex.ru>
2023-04-30 23:44:10 +0300

    Update UserValidator.java
    изменен стиль

--- src/main/java/ru/yandex/practicum/filmorate/validateon/UserValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validateon/UserValidator.java
@@ -8,23 +8,23 @@ import java.time.LocalDate;
 
 @Slf4j
 public class UserValidator {
-    public static void validate(User user){
+    public static void validate(User user) {
 
-        if(user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")){
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
             log.debug("Некорректный email у " + user);
             throw new ValidationException("Некорректный email");
         }
 
-        if(user.getLogin().isBlank() || user.getLogin().contains(" ")){
+        if (user.getLogin().isBlank() || user.getLogin().contains(" ")) {
             log.debug("Неверный логин " + user);
             throw new ValidationException("Неверный логин");
         }
 
-        if(user.getName() == null || user.getName().isEmpty()){
+        if (user.getName() == null || user.getName().isEmpty()) {
             user.setName(user.getLogin());
         }
 
-        if(user.getBirthday().isAfter(LocalDate.now())){
+        if (user.getBirthday().isAfter(LocalDate.now())) {
             log.debug("Дата рождения не будет в будущем " + user);
             throw new ValidationException("Дата рождения не будет в будущем");
         }
