a61aae34a3c540cb2ef51c6245c7f100768f98b0
IvanMikhailovOfficial <ivanm54@yandex.ru>
2023-05-18 02:03:56 +0300

    ТЗ 10

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,41 +1,22 @@
 package ru.yandex.practicum.filmorate.service;
 
-import lombok.extern.slf4j.Slf4j;
-import ru.yandex.practicum.filmorate.errors.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-@Slf4j
-public class FilmService {
-    private Long idGenerator = 1L;
+public interface FilmService {
 
-    private final Map<Long, Film> movieStorage = new HashMap<>();
+    Film addingAMovie(Film film);
 
-    public Film addingAMovie(Film film) {
-        film.setId(idGenerator);
-        movieStorage.put(idGenerator, film);
-        idGenerator++;
-        log.debug("Фильм успешно создан " + film);
-        return film;
-    }
+    List<Film> getAllFilms();
 
-    public List<Film> getAllFilms() {
-        log.debug("Фильмы успешно возвращены");
-        return new ArrayList<>(movieStorage.values());
-    }
+    Film movieUpdate(Film film);
 
-    public Film movieUpdate(Film film) {
-        if (movieStorage.containsKey(film.getId())) {
-            log.debug("Фильм успешно обновлен " + film);
-            movieStorage.put(film.getId(), film);
-            return film;
-        } else {
-            log.debug("Невозможно обновить фильм " + film);
-            throw new ValidationException("Невозможно обновить фильм с таким id");
-        }
-    }
+    Film getById(Long id);
+
+    void addLikeTooFilm(Long id, Long userId);
+
+    void deleteLikeTooFilm(Long id, Long userId);
+
+    List<Film> getPopularFilms(Integer count);
 }
