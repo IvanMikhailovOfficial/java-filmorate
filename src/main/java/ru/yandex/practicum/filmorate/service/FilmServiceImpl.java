0497b787941911ccb5536290b6bf120ef24f5057
IvanMikhailovOfficial <ivanm54@yandex.ru>
2023-05-21 18:08:12 +0300

    Update FilmServiceImpl.java

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmServiceImpl.java
@@ -8,6 +8,7 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 import ru.yandex.practicum.filmorate.validateon.FilmValidator;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -64,7 +65,7 @@ public class FilmServiceImpl implements FilmService {
 
     @Override
     public List<Film> getPopularFilms(Integer count) {
-        return filmStorage.getAllFilms().stream().sorted((a, b) -> b.getLikes().size() - a.getLikes().size()).
-                limit(count).collect(Collectors.toList());
+        return filmStorage.getAllFilms().stream().sorted((a, b) -> b.getLikes().size() - a.getLikes().size())
+                .limit(count).collect(Collectors.toList());
     }
 }
